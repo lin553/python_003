c4385615db8b7c5a92fde22b8066583b862bedd8
iugyf <oeirbios@hotmail.com>
2025-01-12 21:21:40 +0800

    2025-1-12_change

---
+++ p12.py
@@ -0,0 +1,4 @@
+#输出python保留字
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist),'个保留字')
---
+++ p43_常用序列函数.py
@@ -0,0 +1,20 @@
+'''
+x in s          x是否在序列s中，若是返回true
+x not in s      x是否不丰序列s中，若不在返回true
+len(s)          序列s的长度
+max(s)          序列s中最大的数
+min(s)          序列s中最小的数
+s.index(x)      序列s中第一次出现x的位置
+s.count(x)      序列s中x出现的次数
+'''
+
+x='o'
+s='hellow world'
+
+print(x in s)
+print(x not in s)
+print(len(s))
+print(max(s))
+print(min(s))
+print(s.index(x))
+print(s.count(x))
